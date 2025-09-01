5d4f458e035f49ca6d584b2f71c3f252423a41bc
mendi kuperman <mendikuperman@gmail.com>
2025-09-01 11:45:12 +0300

    loader function completed

---
+++ core/loader.py
@@ -0,0 +1,14 @@
+import os.path
+import pandas as pd
+
+
+class DataLoader:
+
+    def __init__(self):
+        base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.path = os.path.join(base_path ,"data","tweets_injected 3.csv")
+
+    def load_data(self):
+        data = pd.read_csv(self.path)
+        return data
+
